7eaaf6a179ed497b0ad4aa6f074b72be530aadd9
mmilushev91 <milushev_1990@abv.bg>
2025-04-23 13:58:47 +0300

    Create getAverage.js

---
+++ learn-introductory-javascript-by-building/getAverage.js
@@ -0,0 +1,13 @@
+/*A teacher has finished grading their students' tests and needs your help to calculate the average score for the class.
+
+Complete the getAverage function which takes in an array of test scores and returns the average score.
+
+The average is calculated by adding up all the scores and dividing by the total number of scores.*/
+function getAverage(scores) {
+  const scoresCount = scores.length;
+  const averageScore = scores.reduce((sum, num) => sum + num, 0) / scoresCount;
+  return averageScore;
+}
+
+console.log(getAverage([92, 88, 12, 77, 57, 100, 67, 38, 97, 89]));
+console.log(getAverage([45, 87, 98, 100, 86, 94, 67, 88, 94, 95]));
